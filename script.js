fccecda3bda5653fcd4c421dc74fca2f02b97f5e
rakitenkogermans <rakitenko.germans@gmail.com>
2022-02-04 22:44:29 +0200

    added leaflet map, App class with constructor and methods, click on map, show form, add marker with popup

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
@@ -10,3 +9,75 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map, mapE;
+
+class App {
+  #map;
+  #mapE;
+
+  constructor() {
+    this._getPosition();
+
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () => {
+        console.log('Something went wrong.')
+      })
+    }
+  }
+
+  _loadMap(pos) {
+
+      const {latitude: lat, longitude: lng} = pos.coords;
+      const coords = [lat, lng];
+
+      this.#map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+      }).addTo(this.#map);
+
+      this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapEvent) {
+    this.#mapE = mapEvent;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(event) {
+    // Prevent from page reload
+    event.preventDefault();
+
+    // Clear input fields
+    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
+
+    const {lat: lt, lng: ln} = this.#mapE.latlng;
+
+    L.marker([lt, ln]).addTo(this.#map)
+      .bindPopup(L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup'
+      }))
+      .setPopupContent('Hello')
+      .openPopup();
+  }
+}
+
+const app = new App();
